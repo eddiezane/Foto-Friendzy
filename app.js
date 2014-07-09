3a0c964e1b3ffb58ce05821c9594c76f80b6ed18
Eddie Zaneski <eddiezane@gmail.com>
2014-07-09 09:20:19 -0400

    All the things

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+// var session = require('express-session');
+// var passport = require('passport');
+// var LocalStrategy = require('passport-local').Strategy;
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
+// var passportLocalMongoose = require('passport-local-mongoose');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,10 +22,61 @@ app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+app.use(cookieParser('tacosareyummy'));
+// app.use(session({secret: 'tacosareyummy'}));
+// app.use(passport.initialize());
+// app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+// Mongoose
+// mongoose.connect('mongodb://localhost/fotofriendzy');
+// var UserSchema = new Schema({});
+// UserSchema.plugin(passportLocalMongoose);
+// var User = mongoose.model('User', UserSchema);
+
+// Passport Local
+// passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+// Routes
+app.get('/', function(req, res) {
+  res.render('new');
+});
+
+// app.get('/new', function(req, res) {
+  // res.render('new', {user: req.user});
+// });
+
+// app.post('/login', passport.authenticate('local'), function(req, res) {
+  // res.redirect('/');
+// });
+
+// app.get('/register', function(req, res) {
+  // res.render('register');
+// });
+
+// app.post('/register', function(req, res) {
+  // User.register(new User({ username : req.body.username }), req.body.password, function(err, account) {
+    // if (err) {
+      // return res.render('register');
+    // }
+    // res.redirect('/');
+  // });
+// });
+
+// Socket Logic
+io.on('connection', function(socket) {
+
+  socket.on('join', function(data) {
+    console.log('Join');
+  });
+
+  socket.on('data', function(data) {
+    console.log("Got data");
+    socket.broadcast.emit('image', data);
+  });
+});
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
